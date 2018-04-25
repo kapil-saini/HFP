53c6475fd4ec27ee165d8179fb62812339e8afe1
kppilsaini <33710389+kppilsaini@users.noreply.github.com>
2018-04-25 16:11:15 +0530

    my first program

---
+++ oddmin.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+odds= range(1,60,2)
+right_this_minute = datetime.today().minute
+
+if right_this_minute in odds:
+    print("this minute seems a little odd")
+else:
+    print("Not an odd  minute")
